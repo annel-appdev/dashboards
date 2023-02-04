f0e4ccd55d41a5d0c4fbbd7e9d5f8becd42b54d9
annenyc <annelau.nyc@gmail.com>
2023-02-04 19:46:39 +0000

    anne-dashboard-final commit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,17 +1,14 @@
 class CurrenciesController < ApplicationController
-
   def first_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
 
-  @raw_data = open("https://api.exchangerate.host/symbols").read
-   @parsed_data = JSON.parse(@raw_data)
-   @symbols_hash = @parsed_data.fetch("symbols")
-  @array_of_symbols = @symbols_hash.keys
-  
-  render({ :template => "currency_templates/step_one.html.erb" })
+    render({ :template => "currency_templates/step_one.html.erb" })
   end
 
   def second_currency
-
     @raw_data = open("https://api.exchangerate.host/symbols").read
     @parsed_data = JSON.parse(@raw_data)
     @symbols_hash = @parsed_data.fetch("symbols")
@@ -21,9 +18,27 @@ class CurrenciesController < ApplicationController
     #Parameters: {"from_currency"=>"AED"}
 
     @from_symbol = params.fetch("from_currency")
-    
 
-  render({ :template => "currency_templates/step_two.html.erb"})
+    render({ :template => "currency_templates/step_two.html.erb" })
   end
 
+  def third_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+
+    #params are
+    #Parameters: {"from_x"=>"USD", "to_y"=>"EUR"}
+
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+
+    @exchange_rate_data = open("https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@to_symbol}").read
+    @exchange_rate_parsed = JSON.parse(@exchange_rate_data)
+    @conversion_hash = @exchange_rate_parsed.fetch("info")
+    @conversion_rate = @conversion_hash.fetch("rate")
+
+    render({ :template => "currency_templates/step_three.html.erb" })
+  end
 end
